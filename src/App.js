662cdee66d0fe01041c1970a2a021fe639d34fd7
Aleksei <pozhitkov.fortech@gmail.com>
2024-10-21 17:31:46 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./css/index.css"
+import Characters from "./Layout/Characters/Characters";
 
 function App() {
   return (
     <div className="App">
-
+      <Characters />
     </div>
   );
 }
---
+++ src/Layout/Characters/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './logo-black 1.svg';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__item">
+                <img src={logo} alt="" />
+            </div>
+            <div className="header__item">
+                <a href="#">Characters</a>
+                <a href="#">Locations</a>
+                <a href="#">Episodes</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
